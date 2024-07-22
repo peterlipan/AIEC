e4c2abe22fee589bda36a19e36189b9c5342a45c
johnP-Li <847041172@qq.com>
2024-07-22 21:14:19 +0800

    debug

--- 3-PLIP/main.py
+++ 3-PLIP/main.py
@@ -8,12 +8,15 @@ import pandas as pd
 import torch.distributed as dist
 import torch.multiprocessing as mp
 import torch.nn as nn
-from torch.nn.parallel import DataParallel
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader
-from dataset import Transforms, PatchDataset
+from dataset import Transforms, PatchDataset, CoordinateDataset
+import torch.nn.functional as F
 from models import CreateModel
 from loss import ProbabilityLoss, BatchLoss, ChannelLoss
+from sklearn.metrics import accuracy_score, f1_score, balanced_accuracy_score, \
+    roc_auc_score, precision_score, matthews_corrcoef, cohen_kappa_score, average_precision_score
+from imblearn.metrics import sensitivity_score, specificity_score
 
 
 def update_ema_variables(student, teacher, alpha, global_step):
@@ -23,13 +26,63 @@ def update_ema_variables(student, teacher, alpha, global_step):
         ema_param.data.mul_(alpha).add_(1 - alpha, param.data)
 
 
-def train(args, train_loader, student_model, teacher_model, optimizer, writer):
-    loss_epoch = 0
+def compute_avg_metrics(ground_truth, activations, avg='micro'):
+    ground_truth = ground_truth.cpu().detach().numpy()
+    activations = activations.cpu().detach().numpy()
+    predictions = np.argmax(activations, -1)
+    mean_acc = accuracy_score(y_true=ground_truth, y_pred=predictions)
+    f1 = f1_score(y_true=ground_truth, y_pred=predictions, average=avg)
+    multi_class = 'ovr'
+    # For binary classification
+    if activations.shape[1] == 2:
+        activations = activations[:, 1]
+        multi_class = 'raise'
+    try:
+        auc = roc_auc_score(y_true=ground_truth, y_score=activations, multi_class=multi_class, average=avg)
+    except ValueError as error:
+        print('Error in computing AUC. Error msg:{}'.format(error))
+        auc = 0
+    try:
+        ap = average_precision_score(y_true=ground_truth, y_score=activations, average=avg)
+    except Exception as error:
+        print('Error in computing AP. Error msg:{}'.format(error))
+        ap = 0
+    bac = balanced_accuracy_score(y_true=ground_truth, y_pred=predictions)
+    sens = sensitivity_score(y_true=ground_truth, y_pred=predictions, average=avg)
+    spec = specificity_score(y_true=ground_truth, y_pred=predictions, average=avg)
+    prec = precision_score(y_true=ground_truth, y_pred=predictions, average=avg)
+    mcc = matthews_corrcoef(y_true=ground_truth, y_pred=predictions)
+    kappa = cohen_kappa_score(y1=ground_truth, y2=predictions)
+
+    return mean_acc, f1, auc, ap, bac, sens, spec, prec, mcc, kappa
+
+
+def validate(dataloader, model):
+    training = model.training
+    model.eval()
+
+    ground_truth = torch.Tensor().cuda()
+    predictions = torch.Tensor().cuda()
+
+    with torch.no_grad():
+        for img, label in dataloader:
+            img, label = img.cuda(non_blocking=True), label.cuda(non_blocking=True).long()
+            _, logits = model(img)
+            pred = F.softmax(logits, dim=1)
+            ground_truth = torch.cat((ground_truth, label))
+            predictions = torch.cat((predictions, pred))
+
+        acc, f1, auc, ap, bac, sens, spec, prec, mcc, kappa = compute_avg_metrics(ground_truth, predictions, avg='macro')
+    model.train(training)
+    return acc, f1, auc, ap, bac, sens, spec, prec, mcc, kappa
+
+
+def train(args, dataloaders, student_model, teacher_model, optimizer, writer):
     probability_loss_func = ProbabilityLoss()
     batch_sim_loss_func = BatchLoss(args.batch_size, args.world_size)
     channel_sim_loss_func = ChannelLoss(args.batch_size, args.world_size)
     ce_loss_func = nn.CrossEntropyLoss()
-    cur_iters = 0
+    train_loader, test_loader = dataloaders
     for step, ((x_i, x_j), label) in enumerate(train_loader):
 
         x_i, x_j, label = x_i.cuda(non_blocking=True), x_j.cuda(non_blocking=True), label.cuda(non_blocking=True).long()
@@ -56,14 +109,24 @@ def train(args, train_loader, student_model, teacher_model, optimizer, writer):
             loss = loss.data.clone()
             dist.all_reduce(loss.div_(dist.get_world_size()))
 
-        cur_iters += 1
-        if args.rank == 0 and cur_iters % 50 == 0:
+        if args.rank == 0 and (step % 50 == 0 or step == len(train_loader) - 1):
             print(f"Step [{step}/{len(train_loader)}]\t Loss: {loss.item()}")
+            test_acc, test_f1, test_auc, test_ap, test_bac, test_sens, test_spec, test_prec, test_mcc, test_kappa = validate(
+                test_loader, student_model)
             if writer is not None:
-                writer.log({'loss': loss.item()})
+                writer.log({'test': {'Accuracy': test_acc,
+                                     'F1 score': test_f1,
+                                     'AUC': test_auc,
+                                     'AP': test_ap,
+                                     'Balanced Accuracy': test_bac,
+                                     'Sensitivity': test_sens,
+                                     'Specificity': test_spec,
+                                     'Precision': test_prec,
+                                     'MCC': test_mcc,
+                                     'Kappa': test_kappa},
+                            'train': {'loss': loss.item(),}}, )
 
-        loss_epoch += loss.item()
-    return loss_epoch
+    return test_acc
 
 
 def main(rank, args, wandb_logger):
@@ -83,7 +146,7 @@ def main(rank, args, wandb_logger):
 
     transforms = Transforms(size=args.size)
 
-    train_dataset = PatchDataset(args.csv_path, transforms=transforms)
+    train_dataset = PatchDataset(args.train_csv_path, transforms=transforms)
 
     # set sampler for parallel training
     if args.world_size > 1:
@@ -103,25 +166,40 @@ def main(rank, args, wandb_logger):
         pin_memory=True,
     )
 
+    if rank == 0:
+        test_dataset = PatchDataset(args.test_csv_path, transforms=transforms.test_transform)
+        test_loader = DataLoader(
+            test_dataset,
+            batch_size=args.batch_size,
+            shuffle=False,
+            drop_last=False,
+            num_workers=args.workers,
+            pin_memory=True,
+        )
+    else:
+        test_loader = None
+
+    dataloaders = (train_loader, test_loader)
+
     n_classes = train_dataset.n_classes
     student = CreateModel(n_classes=n_classes, ema=False).cuda()
     teacher = CreateModel(n_classes=n_classes, ema=True).cuda()
 
-    optimizer = torch.optim.AdamW(student.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(student.parameters(), lr=args.lr)
 
     if args.world_size > 1:
         student = torch.nn.SyncBatchNorm.convert_sync_batchnorm(student)
         student = DDP(student, device_ids=[rank])
     
-    loss_min = 1e9
+    acc = -1
     for epoch in range(args.epochs):
         student.train()
-        loss_epoch = train(args, train_loader, student, teacher, optimizer, wandb_logger)
+        test_acc = train(args, dataloaders, student, teacher, optimizer, wandb_logger)
 
         if args.rank == 0:
-            print(f"Epoch [{epoch}/{args.epochs}]\t Loss: {loss_epoch / len(train_loader)}")
-            if loss_epoch / len(train_loader) < loss_min:
-                loss_min = loss_epoch / len(train_loader)
+            print(f"Epoch [{epoch}/{args.epochs}]\t Accuracy: {test_acc}")
+            if test_acc > acc:
+                acc = test_acc
                 torch.save(student.module.encoder.state_dict(), f"best_{args.backbone}.pth")
 
 
@@ -129,7 +207,8 @@ if __name__ == '__main__':
     # args
     parser = argparse.ArgumentParser()
     parser.add_argument('--visible_gpus', type=str, default='4,5,6,7')
-    parser.add_argument('--csv_path', type=str, default='./patch_info.csv')
+    parser.add_argument('--train_csv_path', type=str, default='./camelyon_training.csv')
+    parser.add_argument('--test_csv_path', type=str, default='./camelyon_testing.csv')
     parser.add_argument('--backbone', type=str, default='resnet50')
     parser.add_argument('--batch_size', type=int, default=24)
     parser.add_argument('--temperature', type=float, default=0.5)
