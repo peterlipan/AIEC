90ba507f83f3928b9c2c64afb2f684d19ed71d84
peterlipan <847041172@qq.com>
2024-07-22 13:01:34 +0000

    debug

--- 3-PLIP/generate_patch_csv.py
+++ 3-PLIP/generate_patch_csv.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 csv_path = '/mnt/zhen_chen/patches_CAMELYON16/status.csv'
 dara_root = '/mnt/zhen_chen/coordinates_CAMELYON16_pruned'
-test_csv = '/mnt/zhen_chen/AIEC/4-MIL/reference.csv'
+test_csv_path = '/mnt/zhen_chen/AIEC/4-MIL/reference.csv'
+test_csv = pd.read_csv(test_csv_path)
 num_levels = 3
 df = pd.DataFrame(columns=['slide_id', 'i', 'j', 'patch_size', 'downsample_factor',
                            'base_level', 'cur_level', 'label'])
@@ -18,7 +19,7 @@ label2num = {'normal': 0, 'tumor': 1}
 for i in tqdm(range(slide_info.shape[0]), desc='WSI', position=0):
     status = slide_info.iloc[i]['status']
     slide_id = slide_info.iloc[i]['slide_id']
-    if status != 'done' or slide_id.startswith('test_'):
+    if True or slide_id.startswith('test_'):
         continue
     slide_name = Path(slide_id).stem
     label = label2num[slide_name.split('_')[0]]
@@ -45,7 +46,8 @@ for i in tqdm(range(slide_info.shape[0]), desc='WSI', position=0):
     if status != 'done' or not slide_id.startswith('test_'):
         continue
     slide_name = Path(slide_id).stem
-    label = label2num[slide_name.split('_')[0]]
+    dx = test_csv[test_csv['slide_id'] == slide_name]['diagnosis'].values[0].lower()
+    label = label2num[dx]
     h5_path = os.path.join(dara_root, f"{slide_name}.h5")
     with h5py.File(h5_path, 'r') as f:
         base_level = f.attrs['base_level']
