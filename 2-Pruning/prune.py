d615f07c1bea0dd1aa909a8f43c31b551069b9dd
peterlipan <847041172@qq.com>
2024-08-02 09:18:24 +0000

    debug

--- 2-Pruning/prune.py
+++ 2-Pruning/prune.py
@@ -87,7 +87,9 @@ def main(rank, csv, args):
             num_pruned = 0
             for level_id in reversed(range(tree.num_levels)):
 
-                threshold = args.threshold / tree.downsample_factor ** level_id ** 2
+                factor = np.prod(tree.downsample_factor[:level_id])
+
+                threshold = args.threshold / factor ** 2
 
                 if args.save:
                     save_dir = os.path.join(heatmap_dir, f'level_{level_id}')
@@ -128,20 +130,20 @@ def main(rank, csv, args):
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument('--csv_path', type=str, default='/mnt/zhen_chen/patches_CAMELYON16_DTFD/status.csv')
+    args.add_argument('--csv_path', type=str, default='/mnt/zhen_chen/patches_AIEC_DTFD_x40_down_2_5/MMRd/status.csv')
     # url: https://tiatoolbox.dcs.warwick.ac.uk/models/seg/fcn-tissue_mask.pth
     args.add_argument('--model_path', type=str, default='./fcn-tissue_mask.pth')
-    args.add_argument('--patch_root', type=str, default='/mnt/zhen_chen/patches_CAMELYON16_DTFD')
-    args.add_argument('--wsi_root', type=str, default='/mnt/zhen_chen/CAMELYON16')
+    args.add_argument('--patch_root', type=str, default='/mnt/zhen_chen/patches_AIEC_DTFD_x40_down_2_5/MMRd')
+    args.add_argument('--wsi_root', type=str, default='/mnt/zhen_chen/AIEC_tiff/MMRd')
     # only to save the coordinates of remained patches after pruning
-    args.add_argument('--save_root', type=str, default='/mnt/zhen_chen/patches_CAMELYON16_DTFD_pruned')
+    args.add_argument('--save_root', type=str, default='/mnt/zhen_chen/patches_AIEC_DTFD_x40_down_2_5_pruned/MMRd')
     args.add_argument('--save', action='store_true')
-    args.add_argument('--batch_size', type=int, default=256)
+    args.add_argument('--batch_size', type=int, default=1024)
     args.add_argument('--patch_size', type=int, default=256)
     args.add_argument('--workers', type=int, default=0)
     args.add_argument('--threshold', type=float, default=0.1, help='Threshold of tissue area at the lowest level')
     args.add_argument('--mode', type=str, default='coordinate')
-    args.add_argument('--visible_gpu', type=str, default='0,1,2,3')
+    args.add_argument('--visible_gpu', type=str, default='2,3,4,5')
     args.add_argument('--port', type=str, default='12345')
     args = args.parse_args()
 
